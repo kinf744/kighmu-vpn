6deaa4d85802a89ddbc6452c02ff44c337311bab
kinf744 <adrienyuoi@gmail.com>
2026-03-06 21:03:27 +0100

    Test ViewModel loading

--- app/src/main/java/com/kighmu/vpn/ui/activities/MainActivity.kt
+++ app/src/main/java/com/kighmu/vpn/ui/activities/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.kighmu.vpn.ui.activities
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import com.kighmu.vpn.ui.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tv = TextView(this)
-        tv.text = "KIGHMU VPN fonctionne!"
-        tv.textSize = 24f
-        setContentView(tv)
+        try {
+            viewModel
+            Toast.makeText(this, "ViewModel OK", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Toast.makeText(this, "ERREUR: " + e.javaClass.simpleName + ": " + e.message, Toast.LENGTH_LONG).show()
+        }
     }
     fun requestVpnConnect() {}
     fun requestVpnDisconnect() {}
