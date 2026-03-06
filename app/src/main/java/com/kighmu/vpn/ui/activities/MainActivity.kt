ff2d9cbeb725ac0b415779fac45e89a66cfdccdc
kinf744 <adrienyuoi@gmail.com>
2026-03-06 21:13:21 +0100

    Test layout inflation

--- app/src/main/java/com/kighmu/vpn/ui/activities/MainActivity.kt
+++ app/src/main/java/com/kighmu/vpn/ui/activities/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.viewModels
+import com.kighmu.vpn.databinding.ActivityMainBinding
 import com.kighmu.vpn.ui.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         try {
-            viewModel
-            Toast.makeText(this, "ViewModel OK", Toast.LENGTH_LONG).show()
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            Toast.makeText(this, "Layout OK", Toast.LENGTH_LONG).show()
         } catch (e: Exception) {
             Toast.makeText(this, "ERREUR: " + e.javaClass.simpleName + ": " + e.message, Toast.LENGTH_LONG).show()
         }
