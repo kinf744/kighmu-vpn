39a9293045a16f278d4d449343f8cee75325637a
kinf744 <adrienyuoi@gmail.com>
2026-03-06 16:59:06 +0100

    Fix SplashActivity crash on launch

--- app/src/main/java/com/kighmu/vpn/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/kighmu/vpn/ui/activities/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.kighmu.vpn.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
@@ -10,9 +11,14 @@ import kotlinx.coroutines.launch
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         lifecycleScope.launch {
             delay(1200)
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
         }
     }
 }
