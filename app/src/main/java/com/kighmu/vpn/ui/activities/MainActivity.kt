80b66ec4e95d42ed997233ce3182b7ad145ef82a
kinf744 <adrienyuoi@gmail.com>
2026-03-06 19:30:53 +0100

    Test bare MainActivity

--- app/src/main/java/com/kighmu/vpn/ui/activities/MainActivity.kt
+++ app/src/main/java/com/kighmu/vpn/ui/activities/MainActivity.kt
@@ -1,53 +1,25 @@
 package com.kighmu.vpn.ui.activities
 
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
+import android.widget.TextView
+import android.graphics.Color
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.kighmu.vpn.R
-import com.kighmu.vpn.databinding.ActivityMainBinding
-import com.kighmu.vpn.ui.MainViewModel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        try {
-            super.onCreate(savedInstanceState)
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            setSupportActionBar(binding.toolbar)
-            val navView: BottomNavigationView = binding.bottomNav
-            val navController = findNavController(R.id.nav_host_fragment)
-            setupActionBarWithNavController(navController)
-            navView.setupWithNavController(navController)
-        } catch (e: Exception) {
-            Toast.makeText(this, "ERREUR: " + e.message, Toast.LENGTH_LONG).show()
-            e.printStackTrace()
-        }
+        super.onCreate(savedInstanceState)
+        val layout = LinearLayout(this)
+        layout.setBackgroundColor(Color.parseColor("#0D1117"))
+        val text = TextView(this)
+        text.text = "MainActivity OK"
+        text.setTextColor(Color.WHITE)
+        text.textSize = 20f
+        layout.addView(text)
+        setContentView(layout)
     }
 
-    fun requestVpnConnect() {
-        try {
-            val permIntent = viewModel.startVpn(this)
-            if (permIntent != null) {
-                startActivityForResult(permIntent, 1)
-            }
-        } catch (e: Exception) {
-            Toast.makeText(this, "VPN Error: " + e.message, Toast.LENGTH_LONG).show()
-        }
-    }
-
-    fun requestVpnDisconnect() {
-        viewModel.stopVpn(this)
-    }
-
-    fun showMessage(msg: String) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-    }
+    fun requestVpnConnect() {}
+    fun requestVpnDisconnect() {}
+    fun showMessage(msg: String) {}
 }
