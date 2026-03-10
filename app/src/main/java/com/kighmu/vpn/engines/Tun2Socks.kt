c27ea14598fee388f0bbb27bc9279b7b9fe86bf1
kinf744 <adrienyuoi@gmail.com>
2026-03-10 01:36:18 +0100

    fix: catch Throwable pour UnsatisfiedLinkError dans terminateTun2Socks

--- app/src/main/java/com/kighmu/vpn/engines/Tun2Socks.kt
+++ app/src/main/java/com/kighmu/vpn/engines/Tun2Socks.kt
@@ -1,20 +1,19 @@
 package com.kighmu.vpn.engines
 
-// Wrapper vers le vrai package JNI
 object Tun2Socks {
     val isAvailable get() = dev.epro.tun2socks.Tun2Socks.isAvailable
 
     fun runTun2Socks(tunFd: Int, mtu: Int, ip: String, prefix: String,
                      socksServerAddress: String, udpgwServerAddress: String,
                      udpgwTransparentDNS: Boolean) {
-        dev.epro.tun2socks.Tun2Socks.runTun2Socks(
-            tunFd, mtu, ip, prefix, socksServerAddress, udpgwServerAddress, udpgwTransparentDNS
-        )
+        try {
+            dev.epro.tun2socks.Tun2Socks.runTun2Socks(
+                tunFd, mtu, ip, prefix, socksServerAddress, udpgwServerAddress, udpgwTransparentDNS
+            )
+        } catch (_: Exception) {}
     }
 
     fun terminateTun2Socks() {
-        if (isAvailable) {
-            try { dev.epro.tun2socks.Tun2Socks.terminateTun2Socks() } catch (_: Exception) {}
-        }
+        try { dev.epro.tun2socks.Tun2Socks.terminateTun2Socks() } catch (_: Throwable) {}
     }
 }
