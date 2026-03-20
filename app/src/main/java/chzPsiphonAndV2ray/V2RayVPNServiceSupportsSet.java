1f3cd6f92b162c3e314f8cd5338f4e8c98da067b
kinf744 <adrienyuoi@gmail.com>
2026-03-20 12:44:45 +0100

    fix: supprimer V2RayVPNServiceSupportsSet.java - utiliser version JAR

--- app/src/main/java/chzPsiphonAndV2ray/V2RayVPNServiceSupportsSet.java
+++
@@ -1,9 +0,0 @@
-package chzPsiphonAndV2ray;
-
-public interface V2RayVPNServiceSupportsSet {
-    long onEmitStatus(long l, String s);
-    long prepare();
-    boolean protect(long fd);
-    long setup(String conf);
-    long shutdown();
-}
