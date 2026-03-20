52e59add5fe9a0720d7d386f2b749e3e5756231d
kinf744 <adrienyuoi@gmail.com>
2026-03-20 11:01:41 +0100

    fix: proxyerror extends Exception pas Error

--- app/src/main/java/go/Universe.java
+++ app/src/main/java/go/Universe.java
@@ -8,9 +8,8 @@ public abstract class Universe {
     private Universe() {}
     public static native void _init();
     public static void touch() {}
-    
-    // proxyerror inner class
-    static final class proxyerror implements java.lang.Error {
+
+    static final class proxyerror extends java.lang.Exception {
         private final int refnum;
         proxyerror(int refnum) { this.refnum = refnum; }
         public native String error();
